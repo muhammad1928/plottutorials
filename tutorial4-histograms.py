74a53139af820cd45b4ad7bc36d1f4d0cdcbbc56
muhammad1928 <muhammadsn7@gmail.com>
2024-10-20 18:17:48 +0200

    histogram

--- tutorial4-histograms.py
+++ tutorial4-histograms.py
@@ -2,5 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# creating 1000 data mean 20 diviation of 1.5
+# creating 1000 data with mean of 20  and diviation of 1.5
 ages = np.random.normal(20,1.5, 1000)
+print(ages)
+
+# creating a histogram plot
+# bins=20 =  how many columns to show in this case 20
+# bins=[ages.min(), 18, 21, ages.max()]  you can also specify the bins urself
+# set cumulative to True to get an icreasing graph
+
+plt.hist(ages, bins=[ages.min(), 18, 21, ages.max()], cumulative=True)
+plt.show()
